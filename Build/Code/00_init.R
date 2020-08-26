8924c948a59a9fc2b64d49a37463436b0ea82a59
cdimke <christine.dimke@gmail.com>
2020-08-26 16:06:10 -0500

    This is the initial commit of the CSFS Economic Resources project.  See the Read me for details

---
+++ Build/Code/00_init.R
@@ -0,0 +1,38 @@
+#This is the preamble script for the Fire Weather project.  
+#It should be run once each time a user begins a new R session to work on the project.
+
+#########################
+#Loading and installing packages
+init.pacs <- function(package.list){
+  check <- unlist(lapply(package.list, require, character.only = TRUE))
+  #Install if not in default library
+  if(any(!check)){
+    for(pac in package.list[!check]){
+      install.packages(pac)
+    }
+    lapply(package.list, require, character.only = TRUE)
+  }
+}
+
+#Loading and installing packages
+init.pacs(c("tidyverse",      #shortcut to many useful packages (eg, dplyr, ggplot)
+            "data.table",     #for more efficient utilities
+            "furrr",          #facilitates simple implementation of parallel processing
+            "future",         #support for furrr
+            "conflicted",     #resolves function conflict across packages
+            "lubridate",      #working with dates
+            "units",
+            "readr",
+            "readxl",
+            "progress",
+            "lwgeom",
+            "haven",
+            "stringr",
+            "mosaic",
+            "purrr",
+            "dbplyr",
+            "RSQLite",
+            "stats"
+))
+
+
---
+++ Build/Code/03_SVI.R
@@ -0,0 +1,10 @@
+## This script takes the raw data and ranks it all creating the final index
+load('./Build/Cache/SVI_data.RData')
+SVI<-SVI_data%>%
+  mutate(directional_median_hh_income_3=-1*median_hh_income_3,.keep="unused")%>%
+  mutate(across(!census_block_group,percent_rank))%>%
+  mutate(overall_sum=rowSums(.[2:16]),
+         overall_rank=percent_rank(overall_sum))
+save(SVI,file='./Build/Index/SVI.RData')
+rm(SVI_data,SVI)
+         
