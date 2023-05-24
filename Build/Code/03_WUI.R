bf4bc76ac36dad0b2f536c28cb7f3ce88ba7c1bf
Vomitadyo <108023789+Vomitadyo@users.noreply.github.com>
2023-05-24 17:29:11 -0600

    script 3 rerun

--- Build/Code/03_WUI.R
+++ Build/Code/03_WUI.R
@@ -1,6 +1,8 @@
 ### This script marks CBGs that contain at least some WUI.  Only these CBGs will be ranked in the WFSVI
 ## Load CO wide dataset
 load('./Build/Cache/SVI_data.RData')
+
+###########################################
 ## add in CO geometries
 load('./Build/Data/state_08.rdata')
 delist<-function(geog){
@@ -8,10 +10,21 @@ delist<-function(geog){
     rename(census_block_group=geoid)%>%
     dplyr::left_join(.,SVI_data)
 }
-SVI_data<-st_out%>%
+st_out<-SVI_data%>%
   map_dfr(.,delist)%>%
   na.omit()
 rm(st_out)
+############################################
+
+##add in CO geometries from a ACS shape file of census block groups.
+CO_geometries <- read_sf("Build/data/CO_geometries/state_08.shp")%>%
+  st_transform(4269)%>%
+  rename(census_block_group=CnssBlG)%>%
+  left_join(.,SVI_data)
+
+
+
+
 ## Load WUI boundary and cut out nonintersecting cbg data and mark intersecting CBGs
 WUI<-read_sf('./Build/Data/2017_wui/2017_wui.shp')%>%
   dplyr::filter(GRIDCODE==1)%>%
