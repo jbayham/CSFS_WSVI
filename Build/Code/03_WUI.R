82e46e2ad1dd41c4f9b92d1287470a9ed890ba5d
Vomitadyo <vomitadyo@gmail.com>
2023-05-29 00:51:31 -0600

    server

--- Build/Code/03_WUI.R
+++ Build/Code/03_WUI.R
@@ -2,28 +2,28 @@
 ## Load CO wide dataset
 load('./Build/Cache/SVI_data.RData')
 
+#Turn Off this section for now
+
 ###########################################
 ## add in CO geometries
-load('./Build/Data/state_08.rdata')
-delist<-function(geog){
-  geog%>%
-    rename(census_block_group=geoid)%>%
-    dplyr::left_join(.,SVI_data)
-}
-st_out<-SVI_data%>%
-  map_dfr(.,delist)%>%
-  na.omit()
-rm(st_out)
-
-
-
+#load('./Build/Data/state_08.rdata')
+#delist<-function(geog){
+  #geog%>%
+    #rename(census_block_group=geoid)%>%
+    #dplyr::left_join(.,SVI_data)
+#}
+#st_out<-SVI_data%>%
+  #map_dfr(.,delist)%>%
+ # na.omit()
+#rm(st_out)
 
 ############################################
 
 ##add in CO geometries from a ACS shape file of census block groups.
 CO_geometries <- read_sf("Build/data/CO_geometries/state_08.shp")%>%
   st_transform(4269)%>%
-  rename(census_block_group=CnssBlG)
+  rename(census_block_group=CnssBlG)%>%
+  select(census_block_group, geometry)
 
 SVI_data_geo <- merge(SVI_data, CO_geometries, all.x=TRUE)%>%#attach geometry to SVI_data
   st_as_sf()%>%
@@ -32,7 +32,7 @@ save(SVI_data_geo, file = './Build/Cache/SVI_data_geo.RData')
 rm(CO_geometries, SVI_data)
 
 ## Load WUI boundary and cut out nonintersecting cbg data and mark intersecting CBGs
-WUI<-read_sf('./Build/Data/2017_wui/2017_wui.shp')%>%
+WUI<-read_sf('Build/data/2017_wui/2017_wui.shp')%>%
   dplyr::filter(GRIDCODE==1)%>%
   st_transform(4269)%>%
   st_make_valid()%>%
@@ -41,9 +41,8 @@ WUI<-read_sf('./Build/Data/2017_wui/2017_wui.shp')%>%
   st_set_geometry(NULL)%>%
   unique()
 
-
 if(!dir.exists("./Build/Cache/")){
   dir.create("./Build/Cache/")
 }     
-save(WUI,file='./Build/Index/WUI_cbg.RData')
+save(WUI,file='./Build/Cache/WUI_cbg.RData')
 rm(SVI_data,WUI)
--- Build/Code/04_dataset.R
+++ Build/Code/04_dataset.R
@@ -4,6 +4,7 @@ load('./Build/Cache/SVI_data.RData')
 load('./Build/Cache/WUI_cbg.RData')
 
 SVI_dat<-SVI_data%>%
+  as.data.frame()%>%
   left_join(.,WUI)%>%
   mutate_at(vars(WUI), ~replace(., is.na(.), 0))
 if(!dir.exists("./Build/ICache/")){
