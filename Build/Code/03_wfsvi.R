7f77663866d85ecc1820f26a5926b71efd33e4de
Jude Bayham <jbayham@gmail.com>
2023-06-09 20:10:59 -0600

    wrote new wui processing routine

--- Build/Code/03_wfsvi.R
+++ Build/Code/03_wfsvi.R
@@ -1,44 +1,66 @@
 ### This script marks CBGs that contain at least some WUI.  Only these CBGs will be ranked in the WFSVI
 ## Load CO wide data set
-load('./Build/Cache/SVI.RData')
+SVI <- readRDS('Build/Cache/SVI.rds')
 
 ##add in geometry for the census block groups from ACS.
 ##download shape file from: https://www.census.gov/cgi-bin/geo/shapefiles/index.php. 
-cbg_geo <- read_sf("Build/data/geometry_08/tl_2022_08_bg.shp")%>%
+###################  
+#Move this chunk to its own script or function because it only needs to be done once.
+download.file(url="ftp://ftp2.census.gov/geo/tiger/TIGER2022/BG/tl_2022_08_bg.zip",
+              destfile = "Build/Cache/tl_2022_08_bg.zip")
+
+unzip("Build/Cache/tl_2022_08_bg.zip",exdir = "Build/Cache/tl_2022_08_bg")
+###################
+cbg_geo <- read_sf("Build/Cache/tl_2022_08_bg/tl_2022_08_bg.shp") %>%
   dplyr::select(GEOID)
 
-SVI_data <- SVI%>%
-  dplyr::left_join(.,cbg_geo)%>%
-  st_as_sf()%>%
-  st_transform(4269)%>%
-  st_make_valid()
-save(SVI_data, file = './Build/Cache/SVI_data.RData')
+#Join SVI to cbg layer
+SVI_geo <- SVI %>%
+  dplyr::left_join(cbg_geo,.)
+
+
+saveRDS(SVI_geo, file = 'Build/Cache/SVI_geo.rds')
 rm(cbg_geo)
 
-## Load WUI boundary and mark CBGs that contain WUI parts.
-WUI<-read_sf('Build/data/2017_wui/2017_wui.shp')%>%
-  dplyr::filter(GRIDCODE==1)%>%
-  st_as_sf()%>%
-  st_transform(4269)%>%
-  st_make_valid()
+SVI_geo <- readRDS('Build/Cache/SVI_geo.rds')
 
-cbg_with_WUI <- st_join(SVI_data, WUI, st_intersects)# intersecting polygons
+###########################
+##
+#This section should be moved to a script (e.g., 03_wui.R)
+##
+#Connect to the raster file
+wui <- rast("Build/Data/WUI.tif") %>% 
+  terra::project(x=.,y=terra::crs(SVI_geo),threads=T)
 
-#since we are only interested in selecting CBGs with a certain WUI part.
-#we are not interested in knowing which specif areas intersetc
-#therefore we can eliminated repeated CBGs 
-unique_cbg <- subset(cbg_with_WUI, !duplicated(cbg_with_WUI$GEOID))
-unique_cbg$part_of_WUI <- ifelse(is.na(unique_cbg$ID), 0, 1)
-wfsvi_cbg <- dplyr::filter(unique_cbg, qualify==1 & part_of_WUI==1)#find final CBGs that qualify
+#reclassify zeros as NA
+NAflag(WUI) <- 0
 
-if(!dir.exists("./Build/Cache/")){
-  dir.create("./Build/Cache/")
-}
+#count the number of non-NA (any WUI) cells in each CBG
+cbg_wui <- exact_extract(wui_reclass,cbg_geo,
+                         'count',append_cols="GEOID",progress=T)
+
+#create flag=1 if there is any WUI in the cbg
+cbg_wui_flag <- cbg_wui %>% 
+  mutate(wui_flag=as.numeric(count>0)) %>%
+  select(-count)
+
+#Join the wui flag back to the SVI data
+wfsvi_cbg <- inner_join(SVI_geo,cbg_wui_flag,by = "GEOID") %>%
+  mutate(qualify_wui=as.numeric((qualify_svi+wui_flag)==2))
+
+
+#Save output layer
+saveRDS(wfsvi_cbg,file='Build/Output/wfsvi_cbg.RData')
+
+
+###################
+#check the fraction of CBGs that qualify and have WUI
+st_set_geometry(wfsvi_cbg,NULL) %>%
+  summarize(check=sum((qualify_svi+wui_flag)==2,na.rm=T)) 
+
+st_set_geometry(wfsvi_cbg,NULL) %>%
+  summarize(check=sum((qualify_svi)==1,na.rm=T))
 
 #Plot the qualifying CBGs with WUI areas inside. . 
 ggplot()+
   geom_sf(data = wfsvi_cbg, fill = 'red', color = 'NA', alpha = 0.5)
-
-save(unique_cbg,file='./Build/Cache/wfsvi_cbg.RData')
-rm(SVI_data,WUI, cbg_with_WUI, unique_cbg, SVI)
-
